b475ce684445918788a210fff00f126341456be0
1ain2 <7703895+1ain2@users.noreply.github.com>
2018-12-21 01:01:20 +0900

    add base functions of todo

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,51 @@
+{-
+    A simple console based todo app.
+-}
+
 module Main where
 
+import           System.IO
+import           System.Directory
+import           Data.List
 
-main :: IO ()
-main = putStr "sss"
+main = do 
+    view ".todo.txt"
+    add ".todo.txt"
+    -- complete ".todo.txt"
+
+    -- Todo: /で各コマンドを入力できるようにする
+    -- command <- getLine
+    -- dispatch command
+
+
+view :: String -> IO ()
+view fileName = do
+    contents <- readFile fileName
+    let todoTasks = lines contents
+        numberedTasks = zipWith (\n line -> show n ++ " " ++ line) [0..] todoTasks
+    putStrLn "Todo List"
+    putStr $ unlines numberedTasks
+
+
+add :: String -> IO ()
+add fileName = do
+    putStrLn "add task ..."
+    todoItem <- getLine
+    appendFile fileName ("- [ ] " ++ todoItem ++ "\n")
+
+
+complete :: String -> IO ()
+complete fileName = do
+    contents <- readFile fileName
+    let todoTasks = lines contents
+
+    putStrLn "select complete task ..."
+    numberString <- getLine
+    -- TODO: todoを削除するのではなく、xを付与する
+    let number = read numberString
+        newTodoItems = unlines $ delete (todoTasks !! number) todoTasks
+    (tempName, tempHandle) <- openTempFile "." "temp"
+    hPutStr tempHandle newTodoItems
+    hClose tempHandle
+    removeFile fileName
+    renameFile tempName fileName
